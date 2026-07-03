a477d438e0c3027fb5b4edec365956d98194211d
kfigon <kamil.figon@gmail.com>
2026-07-03 14:57:22 +0200

    different direction

--- fahrenheit.c
+++ fahrenheit.c
@@ -8,7 +8,9 @@ int main() {
     printf("fahr\tcelc\n");
 
     // init*, condition, loop, increment, repeat
+
     for (int fahr = low; fahr <= high; fahr += step) {
+    // for (int fahr = high; fahr >= low; fahr -= step) {
         float celc = (5.0/9.0) * (fahr-32);
         printf("%4d %6.1f\n", fahr, celc);
     }
