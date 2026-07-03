66200d067c8506886d02c4c3d1bb2240789268b9
kfigon <kamil.figon@gmail.com>
2026-07-03 14:39:54 +0200

    formatting

--- fahrenheit.c
+++ fahrenheit.c
@@ -5,10 +5,11 @@ int main() {
     const int high = 300;
     const int step = 20;
     
+    printf("fahr\tcelc\n");
     int fahr = low;
     while (fahr <= high) {
         int celc = 5* (fahr-32)/9;
-        printf("%d fahr \t %d celc\n", fahr, celc);
+        printf("%d\t%d\n", fahr, celc);
         fahr += step;
     }
     return 0;
