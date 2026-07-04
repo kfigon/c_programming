97e1f868e3753fef3af8c2735d483a1a569367bf
kfigon <kamil.figon@gmail.com>
2026-07-04 23:40:24 +0200

    word_per_line

---
+++ word_per_line.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int c;
+    int in_word = 0;
+    while ((c = getchar()) != EOF) {
+        int is_space = c == ' ' || c == '\t' || c == '\n';
+
+        // is_space | in_word
+        // 0           0 -> enter word, print_char
+        // 0           1 -> print c
+        // 1           0 -> skip
+        // 1           1 -> leave word, print newline
+        if(is_space) {
+            if (in_word){
+                in_word = 0;
+                putchar('\n');
+            }
+        } else if (in_word){
+            putchar(c);
+        } else {
+            in_word = 1;
+            putchar(c);
+        }
+
+    }
+    return 0;
+}
