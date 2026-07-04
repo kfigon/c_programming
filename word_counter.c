38cc29a7dcdddff8a31287510c9dbb350884d475
kfigon <kamil.figon@gmail.com>
2026-07-04 23:27:40 +0200

    word counter

--- word_counter.c
+++ word_counter.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+
+#define IN_WORD 1
+#define OUT_WORD 0
+
 int main() {
-    int c;
-    int word_count= 0;
+    int c, state, char_count, word_count, line_count;
+
+    state = OUT_WORD;
+    char_count = word_count = line_count = 0;
+
     while ((c = getchar()) != EOF){
-        
+        char_count++; 
+        if (c == ' ' || c == '\t' || c == '\n') {
+            state = OUT_WORD;
+            if (c == '\n') line_count++;
+        } else if(state == OUT_WORD) {
+            state = IN_WORD;
+            word_count++;
+        }
     }
-    printf("%d\n", word_count);
+    printf("wc: %d, lc: %d, cc: %d\n", word_count, line_count, char_count);
     return 0;
 }
