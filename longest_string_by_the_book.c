3e3837587882bae22bab40058e0fc649038f75c0
kfigon <kamil.figon@gmail.com>
2026-07-06 13:33:19 +0200

    longest string by the book

---
+++ longest_string_by_the_book.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define MAX_LINE 100
+
+void copy(char src[], char dest[]);
+int get_line(char out[]);
+
+int main() {
+    char line[MAX_LINE];
+    char longest[MAX_LINE];
+    int max = 0;
+    int current_len = 0;
+
+    for(int i = 0; i< MAX_LINE; i++){
+        line[i] = 0;
+        longest[i] = 0;
+    }
+
+    while((current_len = get_line(line)) > 0) {
+        if (current_len > max){
+            copy(line, longest);
+            max = current_len;
+        }
+    }
+
+    printf("longest: \"%s\"\n", longest);
+    return 0;
+}
+
+void copy(char src[], char dest[]) {
+    int i = 0;
+    while(i < MAX_LINE-1 && src[i] != 0){
+       dest[i] = src[i];
+       i++;
+    }
+    dest[i] = 0;
+}
+int get_line(char out[]){
+    int len = 0;
+    int c;
+    while ((c = getchar()) != EOF && c != '\n' && len < MAX_LINE - 1) {
+        out[len] = c;
+        len++;
+    }
+
+    out[len] = 0;
+    return len;
+}
+
